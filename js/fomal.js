3ddc64d05a4a965b216859ef4990bcfedc97b6dd
Britneyks <britneyzhcn@gmail.com>
2024-03-11 11:29:40 +0800

    Site updated: 2024-03-11 11:29:31

--- js/fomal.js
+++ js/fomal.js
@@ -1283,8 +1283,8 @@ function share_() {
   try {
     // 截取标题
     var title = document.title;
-    var subTitle = title.endsWith("| Fomalhaut🥝") ? title.substring(0, title.length - 14) : title;
-    navigator.clipboard.writeText('Fomalhaut🥝的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
+    var subTitle = title.endsWith("| 子非鱼🐟") ? title.substring(0, title.length - 14) : title;
+    navigator.clipboard.writeText('子非鱼🐟的站内分享\n标题：' + subTitle + '\n链接：' + url + '\n欢迎来访！🍭🍭🍭');
     new Vue({
       data: function () {
         this.$notify({
